627f52a2c05af35bf4376b25f74c8d34be3ce658
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-29 14:47:07 +0300

    Поиск максимального значения

--- OverloadFunctions.cpp
+++ OverloadFunctions.cpp
@@ -15,11 +15,27 @@ using namespace std;
 void FillArray(int arr[], int size);
 
 /// <summary>
+/// Перегруженный метод заполнения двумерного массива
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="rows"></param>
+/// <param name="cols"></param>
+void FillArray(int arr[][20], int rows, int cols);
+
+/// <summary>
 /// Вывод массива на экран
 /// </summary>
 /// <param name="arr"></param>
 /// <param name="size"></param>
 void PrintArray(int arr[], int size);
+
+/// <summary>
+/// Перегруженный метод печати двумерного массива
+/// </summary>
+/// <param name="arr"></param>
+/// <param name="rows"></param>
+/// <param name="cols"></param>
+void PrintArray(int arr[][20], int rows, int cols);
 
 /// <summary>
 /// Нахождение максимального значения в массиве
@@ -36,7 +52,7 @@ int FindMax(int arr[], int size);
 /// <param name="rows"></param>
 /// <param name="cols"></param>
 /// <returns></returns>
-int FindMax(const int arr[][3], int rows, int cols);
+int FindMax(int arr[][20], int rows, int cols);
 
 int main()
 {
@@ -44,12 +60,19 @@ int main()
     SetConsoleOutputCP(1251);
 
     const int size = 20;
+    const int rows = 20;
+    const int cols = 20;
     int Array[size];
+    int TwoDimensionalArray[rows][cols];
     
     FillArray(Array, size);
-    cout << "Заполненный массив : " << endl;
+    cout << "Заполненный одномерный массив : " << endl;
     PrintArray(Array, size);
     cout << "Максимальное значение в массиве : " << FindMax(Array, size) << endl;
+    cout << "Заполненный двумерный массив : " << endl;
+    FillArray(TwoDimensionalArray, rows, cols);
+    PrintArray(TwoDimensionalArray, rows, cols);    
+    cout << "Максимальное значение в двумерном в массиве : " << FindMax(TwoDimensionalArray, rows, cols) << endl;
 }
 
 
@@ -61,6 +84,17 @@ void FillArray(int arr[], int size) {
     }
 }
 
+
+void FillArray(int arr[][20], int rows, int cols) {
+    random_device random;
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            arr[i][j] = random() % (100 + 50) - 50;
+        }
+    }
+}
+
+
 void PrintArray(int arr[], int size) {
     for (size_t i = 0; i < size; i++)
     {
@@ -70,12 +104,32 @@ void PrintArray(int arr[], int size) {
     cout << endl;
 }
 
+void PrintArray(int arr[][20], int rows, int cols) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << "[" << arr[i][j] << "] ";
+        }
+        cout << std::endl;
+    }
+}
 
 int FindMax(int arr[], int size) {
     int maxVal = arr[0];
     for (int i = 1; i < size; ++i) {
         if (arr[i] > maxVal) {
             maxVal = arr[i];
+        }
+    }
+    return maxVal;
+}
+
+int FindMax(int arr[][20], int rows, int cols) {
+    int maxVal = arr[0][0];
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            if (arr[i][j] > maxVal) {
+                maxVal = arr[i][j];
+            }
         }
     }
     return maxVal;
